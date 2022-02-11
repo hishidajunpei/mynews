e3d10f98976ccbf774febf6a7d938eca2c7894f7
j-hishida <1279059@gmail.com>
2022-02-11 14:27:19 +0000

    課題13

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,12 @@ Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 Auth::routes();
