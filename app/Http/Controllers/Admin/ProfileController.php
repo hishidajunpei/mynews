e8e03a169b713ac17b65a54bb3ed24150c27c4e2
j-hishida <1279059@gmail.com>
2022-02-23 14:12:53 +0000

    課題16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
@@ -12,21 +11,56 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        $profile->fill($form);
-        $profile->save();
-        return redirect('admin/profile/create');
+       $this->validate($request, Profile::$rules);
+       $profile = new Profile;
+       
+       $form = $request->all();
+       
+       unset($form['_token']);
+       
+       $profile->fill($form);
+       $profile->save();
+       
+       return redirect('admin/profile/create');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+        abort(404);    
+      }
+        return view('admin.profile.edit', ['profile_form' => $profile]); 
     }
 
-    public function update()
+    public function update(Request $request)
     {
-        
-        return redirect('admin/profile/edit');
-    }
+      $this->validate($request, Profile::$rules);
+      
+      $profile = Profile::find($request->id);
+    
+      $profile_form = $request->all();
+      unset($profile_form['_remove']);
+      unset($plofile_form['_token']);
+
+      $plofile->fill($plofile_form)->save();
+
+      return redirect('admin/profile');
+     }
+      public function delete(Request $request)
+    {
+      $plofile = Profile::find($request->id);
+      
+      $plofile->delete();
+      return redirect('admin/profile/');
+    }  
+
+
 }
+
+        
+        
+        
+
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,10 @@ Route::group(['prefix' => 'admin'], function() {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+    Route::get('profile','Admin\ProfileController@indiex')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+    Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
 });
 
 Auth::routes();
@@ -37,4 +39,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
