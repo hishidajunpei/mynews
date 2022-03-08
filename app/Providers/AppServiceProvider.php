8635e35e5b0fb12bf28dfdace93005e7417a84fc
j-hishida <1279059@gmail.com>
2022-03-08 03:36:10 +0000

    課題19

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //以下を追記
         if (\App::environment('production')) {
-            \URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 }
